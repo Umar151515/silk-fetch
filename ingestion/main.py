be51621d73ffc779ee98c3465d5bf1840761676d
Umar Sovetbekov <umarsovetbekov2@gmail.com>
2026-07-30 20:25:13 +0600

    refactor(ingestion): delegate control from pyrogram to faststream

--- ingestion/main.py
+++ ingestion/main.py
@@ -1,11 +1,8 @@
-import asyncio
-
 from pyrogram import Client
-from faststream import FastStream
+from faststream import FastStream, ContextRepo
 from faststream.redis import RedisBroker
 
 from shared.core.config import config
-
 
 broker = RedisBroker(config.redis_url)
 faststream_app = FastStream(broker)
@@ -17,7 +14,11 @@ bot = Client(
     plugins=dict(root="ingestion")
 )
 
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(faststream_app.run())
-    bot.run()
+@broker.on_startup
+async def start_bot(context: ContextRepo):
+    await bot.start()
+    context.set_global("bot", bot)
+
+@broker.on_shutdown
+async def stop_bot():
+    await bot.stop()
